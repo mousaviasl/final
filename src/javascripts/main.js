31a63488b7db22bc6f6584cbc359484d7ca43dad
Esmaeil Mousavi <97320038+mousaviasl@users.noreply.github.com>
2023-12-15 07:04:50 +0000

    Cs

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,2 +1,43 @@
 //TODO - Your ES6 JavaScript code (if any) goes here
 import "bootstrap"
+//TODO - Your ES6 JavaScript code (if any) goes here
+import "bootstrap"
+
+document.addEventListener('DOMContentLoaded', function () {
+    const searchForm = document.querySelector('.navbar .d-flex');
+
+    searchForm.addEventListener('submit', function (event) {
+        event.preventDefault();
+        const searchInput = document.querySelector('.navbar input[type="search"]');
+        const searchTerm = searchInput.value.trim();
+
+        console.log('Search Term:', searchTerm);
+    });
+});
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const mailingListForm = document.getElementById('mailingListForm');
+    let subscriptionList = [];
+
+    mailingListForm.addEventListener('submit', function (event) {
+        event.preventDefault(); 
+
+        const name = document.getElementById('name').value.trim();
+        const email = document.getElementById('email').value.trim();
+
+        const person = { name: name, email: email };
+        subscriptionList.push(person);
+
+        console.log('Mailing List Subscription:', person);
+        console.log('Updated Subscription List:', subscriptionList);
+
+        document.getElementById('name').value = '';
+        document.getElementById('email').value = '';
+
+    });
+});
+
+
+
+
